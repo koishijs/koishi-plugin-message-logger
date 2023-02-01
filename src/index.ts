cf9f91ca5973de8d0631c081516e216eec45185b
Seidko <64234553+Seidko@users.noreply.github.com>
2023-02-01 11:41:29 +0800

    chore: code optimization

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,16 @@ const logger = new Logger('message-logger')
 export const name = 'message-logger'
 
 export interface Config {}
-
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.middleware(async (session, next) => {
-    switch (session.subtype) {
-      case 'private': {
-        const user = await session.bot.getUser(session.userId)
-        logger.info(`收到来自于 ${session.platform} 平台 ${user.username}(${user.userId}) 的私聊消息：${session.content}`)
-      }
-      break
-      case 'group': {
-        const guild = await session.bot.getGuild(session.guildId)
-        const user = await session.bot.getGuildMember(session.guildId, session.userId)
-        logger.info(`收到来自于 ${session.platform} 平台 ${guild.guildName}(${guild.guildId}) 成员 ${user.username}(${user.userId}) 的群聊消息：${session.content}`)
-      }
+    if (!session.guildId) logger.info(`收到来自于 ${session.platform} 平台 ${session.username}(${session.userId}) 的私聊消息：${session.content}`)
+    else try {
+      const guild = await session.bot.getGuild(session.guildId)
+      logger.info(`收到来自于 ${session.platform} 平台 ${guild.guildName}(${guild.guildId}) 成员 ${session.username}(${session.userId}) 的群聊消息：${session.content}`)
+    } catch {
+      logger.info(`收到来自于 ${session.platform} 平台 未命名群组(${session.guildId}) 成员 ${session.username}(${session.userId}) 的群聊消息：${session.content}`)
     }
     return next()
   }, true)
